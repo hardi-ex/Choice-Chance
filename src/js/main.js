6d62bde6e1583fc42342673a99ac6cfd29f3e05e
Yaroslav <skylandtat@ukr.net>
2024-05-21 23:12:30 +0200

    f6

--- src/js/main.js
+++ src/js/main.js
@@ -44,12 +44,12 @@ startBtn.addEventListener('click', () => {
     // const isWinner = smiles.every(el => el === '🤑');
     const isWinner = !smiles.includes('🖕');
     if (isWinner) {
-      result.classList.remove('result');
       result.classList.add('result-win');
+      result.classList.remove('result');
       result.textContent = `Drink!`;
     } else {
-      result.classList.remove('result');
       result.classList.add('result-lose');
+      result.classList.remove('result');
       result.textContent = 'Home';
     }
     startBtn.disabled = false;
@@ -59,9 +59,9 @@ startBtn.addEventListener('click', () => {
     for (let i = 0; i < 3; i++) {
       container.children[i].textContent = '';
     }
+    result.classList.add('result');
     result.classList.remove('result-lose');
     result.classList.remove('result-win');
-    result.classList.add('result');
     result.textContent = 'Result';
   }, 5000);
 });
